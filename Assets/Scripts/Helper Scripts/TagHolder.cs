9e88042d28a99de69075b7272efccd0b13446e0a
Fauzan Fathurrahman <fauzan08fauzan@gmail.com>
2020-12-10 07:52:45 +0700

    Player Movement + Asset (Educational Only)

---
+++ Assets/Scripts/Helper Scripts/TagHolder.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis {
+    public const string HORIZONTAL = "Horizontal";
+    public const string VERTICAL = "Vertical";
+}
